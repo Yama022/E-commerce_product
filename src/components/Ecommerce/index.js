8c0ae4009d8ed406a161706c144fa0f440959ea6
Yama022 <r-jacquemin@hotmail.fr>
2022-01-12 14:22:08 +0100

    Mise en place composant Header + scss

--- src/components/Ecommerce/index.js
+++ src/components/Ecommerce/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import Header from '../Header';
 import './style.scss';
 
 export default function Ecommerce() {
   return (
-    <div className="ecommerce">Ecommerce</div>
+    <div className="ecommerce">
+      <Header />  
+      Ecommerce
+      
+    </div>
   );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './style.scss';
+
+export default function Header() {
+  return (
+    <div className="header">sneakers
+    
+    </div>
+  );
+}
